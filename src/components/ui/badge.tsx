35c34a7c62a6dfda4effa753dc9fb574f2a0370c
momotaro <momotaro.n@gmail.com>
2025-07-09 20:45:20 +0900

    feat: アイドルバッジの色が変更されない問題を修正

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -3,17 +3,6 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
 
-const variant = ['mofcro', 'girudoru', 'mofrurock', 'osahoto'].reduce(
-  (ret, id) => {
-    ret[id] =
-      `border-transparent bg-${id}-bg text-${id}-fg hover:bg-${id}-bg/80`;
-    ret[`${id}-outline`] = `border-${id}-text text-${id}-text`;
-    return ret;
-  },
-  {}
-);
-console.log(variant);
-
 const badgeVariants = cva(
   'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
@@ -21,7 +10,18 @@ const badgeVariants = cva(
       variant: {
         default:
           'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
-        ...variant,
+        mofcro:
+          'border-transparent bg-mofcro-bg text-mofcro-fg hover:bg-mofcro-bg/80',
+        'mofcro-outline': 'border-mofcro-text text-mofcro-text',
+        girudoru:
+          'border-transparent bg-girudoru-bg text-girudoru-fg hover:bg-girudoru-bg/80',
+        'girudoru-outline': 'border-girudoru-text text-girudoru-text',
+        mofrurock:
+          'border-transparent bg-mofrurock-bg text-mofrurock-fg hover:bg-mofrurock-bg/80',
+        'mofrurock-outline': 'border-mofrurock-text text-mofrurock-text',
+        osahoto:
+          'border-transparent bg-osahoto-bg text-osahoto-fg hover:bg-osahoto-bg/80',
+        'osahoto-outline': 'border-osahoto-text text-osahoto-text',
       },
     },
     defaultVariants: {
