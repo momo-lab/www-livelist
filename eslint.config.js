4c4c06a6e63fadd8721d53c53c57bfa3fdfa9f75
momotaro <momotaro.n@gmail.com>
2025-12-15 16:27:41 +0900

    chore: ツール、ライブラリのアップデート

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,31 @@
 import js from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config([
+export default [
   globalIgnores(['dist']),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactRefresh.configs.vite,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    plugins: {
+      'react-hooks': reactHooks,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+
     rules: {
-      'react-refresh/only-export-components': 'off', // このルールを無効化
+      'react-refresh/only-export-components': 'off',
+      // Hooksのルールを有効化
+      'react-hooks/rules-of-hooks': 'error',
+      // 依存配列の警告を有効化
+      'react-hooks/exhaustive-deps': 'warn',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
@@ -31,5 +35,14 @@ export default tseslint.config([
       ],
     },
   },
-  eslintPluginPrettierRecommended,
-]);
+  {
+    files: ['**/*.config.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+];
