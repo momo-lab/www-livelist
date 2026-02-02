fd409996ef866e96ce9b96342e506d00313a0365
momotaro <momotaro.n@gmail.com>
2026-02-02 14:17:05 +0900

    refactor: skeleton.tsxを最新化

--- src/components/LiveEventTableSkeleton.tsx
+++ src/components/LiveEventTableSkeleton.tsx
@@ -7,26 +7,23 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { cn } from '@/lib/utils';
 
 export const LiveEventTableSkeleton = () => {
   return (
-    <Table>
-      <TableHeader className={cn('bg-header-bg', 'text-header-fg')}>
+    <Table className="border-separate border-spacing-0 rounded-lg border">
+      <TableHeader className="bg-header-bg text-header-fg">
         <TableRow>
-          <TableHead className="border-border w-20 rounded-tl-lg border-r text-center">
-            日付
-          </TableHead>
-          <TableHead className="rounded-tr-lg">イベント内容</TableHead>
+          <TableHead className="w-20 rounded-tl-lg border-r border-b text-center">日付</TableHead>
+          <TableHead className="rounded-tr-lg border-b">イベント内容</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {[...Array(10)].map((_, i) => (
           <TableRow key={i}>
-            <TableCell className="border-border w-20 border-r p-2 text-center align-top">
+            <TableCell className="w-20 border-r border-b p-2 text-center align-top">
               <Skeleton className="h-6 w-full" />
             </TableCell>
-            <TableCell>
+            <TableCell className="border-b">
               <div className="space-y-2 px-4 py-2">
                 <Skeleton className="h-4 w-1/4" />
                 <Skeleton className="h-8 w-full" />
